f2602cc1386d79ca4a6695c4c5f1db60105612a1
Moe <mohamed.alsharif@neufund.org>
2017-09-22 02:45:06 +0200

    Scrape smart contract ABI from etherscan Store ABI locally Tests

--- src/config.js
+++ src/config.js
@@ -2,8 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/**
+ * getSocketPortNumber will return the port number from .env file 
+ */
+
 export const getSocketPortNumber = () => parseInt(process.env.SOCKET_IO_PORT, 10);
 
 export const getLogLevel = () => process.env.LOG_LEVEL;
 
 export const getRPCNode = () => process.env.RPC_URL;
+
+export const getAccessToken = () => process.env.ACCESS_TOKEN;
