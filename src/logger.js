e88a62f5c8a085334cdd67fe19921239319aaeba
mostafabalata <mostafa.balata@gmail.com>
2017-09-28 18:28:53 +0200

    format the output

--- src/logger.js
+++ src/logger.js
@@ -4,9 +4,9 @@ import { getLogLevel } from './config';
 export default new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
-      timestamp: true,
+      timestamp: false,
       level: getLogLevel(),
-      colorize: true,
+      colorize: false,
+      formatter: options => `${options.message}`,
     }),
-  ],
-});
+  ] });
--- src/output/index.js
+++ src/output/index.js
@@ -1,9 +1,31 @@
 import logger from '../logger';
 
 export default (data) => {
-  logger.info(`
-  txHash: ${data.transaction.hash}
-  decoded input: ${JSON.stringify(data.decodedInputDataResult)}
-  decoded logs: ${JSON.stringify(data.decodedLogs)}
-  `);
+  const txHash = data.transaction.hash;
+  let functionName = '';
+  let functionParams;
+  let eventText = '';
+  let extraMessage = '';
+  if (data.decodedInputDataResult) {
+    functionName = data.decodedInputDataResult.name;
+    functionParams = data.decodedInputDataResult.params.map(param =>
+      `${param.name}=${param.value}`);
+  }
+
+  if (data.decodedLogs) {
+    data.decodedLogs.forEach((log) => {
+      eventText = `${log.name}(`;
+      log.events.forEach((i, idx, events) => {
+        eventText += `${events[idx].name}=${events[idx].value}`;
+        if (idx !== events.length - 1) {
+          eventText += ',';
+        }
+      });
+      eventText += ')';
+    });
+  }
+  if (data.transaction.gas === data.transaction.gasUsed) {
+    extraMessage = 'Suspected fail';
+  }
+  logger.info(`tshash:${txHash} ${functionName}(${functionParams}) ${eventText} ${extraMessage}`);
 };
