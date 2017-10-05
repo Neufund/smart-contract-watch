17eddae104cbb5360ad6ed3f5035e2b34b61c923
Moe <mohamed.alsharif@neufund.org>
2017-10-05 18:53:54 +0200

    enhance decoding module
    add getDecodedAddresses
    add extra error handeling
    add tests

--- test/decoder.spec.js
+++ test/decoder.spec.js
@@ -1,19 +1,34 @@
-import path from 'path';
-import fs from 'fs';
 import { expect } from 'chai';
-import { addABI, decodeLogData, decodeInputData } from '../src/decoder';
+import { addABI, decodeLogData, decodeInputData, getDecodedAddresses } from '../src/decoder';
+import { mockABI } from './mockedData/mockABI';
 
-
-describe('decodeInputData', () => {
-  const esopjson = path.join(__dirname, 'mockedData', 'esopABI.json');
-  const abi = JSON.parse(fs.readFileSync(esopjson, { encoding: 'utf8' }));
-  addABI(abi);
-
-  it('should return function name and its params for the transaction', () => {
-    const dataSample1 = '0xcc96b9430000000000000000000000006a58a4e262b6ead9eb40f41313d6d1fbbbb41c160000000000000000000000000000000000000000000000000000000057fabde0000000000000000000000000000000000000000000000000000000005960cc0600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001';
-    const openESOP = {
-      name: 'offerOptionsToEmployee',
-      params:
+describe('Decoder Module', () => {
+  const esopAddress = '0xda7c27c04f66842faf20644814b644e25e1766ea';
+  const invalidAddress = '0x6a58a4e262b6ead9eb40f41313d6d1fbbb41c16';
+  const nonAddedABIaddress = '0xa74476443119A942dE498590Fe1f2454d7D4aC0d';
+  describe('getDecodedAddresses', () => {
+    it('should return an empty array', () => {
+      expect(getDecodedAddresses()).to.have.lengthOf(0);
+    });
+    it('should return an array with added address', () => {
+      addABI(mockABI, esopAddress);
+      expect(getDecodedAddresses()).to.deep.include(esopAddress);
+    });
+  });
+  describe('addABI', () => {
+    it('should throw if address was invalid', () => {
+      expect(() => addABI(mockABI, invalidAddress)).to.throw('Input is not an address');
+    });
+    it('should throw if ABI was undefined', () => {
+      expect(() => addABI(null, esopAddress)).to.throw('can\'t add undefined/null ABI');
+    });
+  });
+  describe('decodeInputData', () => {
+    it('should return function name and its params for transaction', () => {
+      const dataSample1 = '0xcc96b9430000000000000000000000006a58a4e262b6ead9eb40f41313d6d1fbbbb41c160000000000000000000000000000000000000000000000000000000057fabde0000000000000000000000000000000000000000000000000000000005960cc0600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001';
+      const openESOP = {
+        name: 'offerOptionsToEmployee',
+        params:
      [{ name: 'e',
        value: '0x6a58a4e262b6ead9eb40f41313d6d1fbbbb41c16',
        type: 'address' },
@@ -21,22 +36,40 @@ describe('decodeInputData', () => {
      { name: 'timeToSign', value: '1499515910', type: 'uint32' },
      { name: 'extraOptions', value: '0', type: 'uint32' },
      { name: 'poolCleanup', value: true, type: 'bool' }] };
-    const decodedInput = decodeInputData(dataSample1);
-
-    expect(decodedInput).to.deep.equal(openESOP);
+      const decodedInput = decodeInputData(dataSample1, esopAddress);
+      expect(decodedInput).to.deep.equal(openESOP);
+    });
+    it('should return encoded if the ABI for this specific address is not added', () => {
+      const dataSample1 = '0000000000000000000000000c53fe380aba335d144b6f0dbc6b588633f783d7000000000000000000000000f3a85b1c8818629e52d61';
+      const decodedInput = decodeInputData(dataSample1, nonAddedABIaddress);
+      expect(decodedInput).to.deep.equal('0000000000000000000000000c53fe380aba335d144b6f0dbc6b588633f783d7000000000000000000000000f3a85b1c8818629e52d61');
+    });
+    it('should throw if there was an error during decoding', () => {
+      const dataSampleErr = 'a58a4e262b6ead9eb4';
+      expect(() => decodeInputData(dataSampleErr, esopAddress)).to.throw('problem during decoding');
+    });
+    it('should throw if address/data is not sent', () => {
+      const dataSampleErr = 'a58a4e262b6ead9eb4';
+      expect(() => decodeInputData(dataSampleErr)).to.throw('decodedData or address are undefined');
+    });
+    it('should throw if address was an invalid address', () => {
+      const dataSampleErr = 'a58a4e262b6ead9eb4';
+      expect(() => decodeInputData(dataSampleErr, invalidAddress)).to.throw('decodedData or address are undefined');
+    });
   });
-
-  it('should throw if input data doesnt decode', () => {
-    const dataSampleErr = 'a58a4e262b6ead9eb4';
-    expect(() => decodeInputData(dataSampleErr)).to.throw('Problem with input data');
-  });
-
-  it('should return decoded logs and its params from a transaction', () => {
+  describe('decodeLogData', () => {
     const testLogs = [
       {
         data: '',
         topics: ['0x06155e9532ee6dc12fad75fb22ffb46f42fc8c2a6342389c1ac5776da7deab10', '0x00000000000000000000000003158526a0264cc2a83dd18c56d4fe8e612b2dc1'],
-        address: '0xda7c27c04f66842faf20644814b644e25e1766ea',
+        address: esopAddress,
+      },
+    ];
+    const errTestLogs = [
+      {
+        data: '0x000000e6dc12fad75fb22ffb46f42fc8c2a6342389c1ac5776da7deab10',
+        topics: ['0x06155e9532ee6dc12fad75fb22ffb46002fc8c2a6342389c1ac5776da7deab10', '0x00000000000000000000000883158526a0264cc2a83dd18c56d4fe8e612b2dc1'],
+        address: esopAddress,
       },
     ];
     const esopEvent = [{ name: 'EmployeeSignedToESOP',
@@ -47,8 +80,23 @@ describe('decodeInputData', () => {
           value: '0x03158526a0264cc2a83dd18c56d4fe8e612b2dc1',
         },
       ],
-      address: '0xda7c27c04f66842faf20644814b644e25e1766ea' }];
-    const decodedInput = decodeLogData(testLogs);
-    expect(decodedInput).to.deep.equal(esopEvent);
+      address: esopAddress }];
+    it('should return decoded logs and its params from a transaction', () => {
+      const decodedInput = decodeLogData(testLogs, esopAddress);
+      expect(decodedInput).to.deep.equal(esopEvent);
+    });
+    it('should return encoded if the ABI for this specific address is not added', () => {
+      const decodedInput = decodeLogData(errTestLogs, nonAddedABIaddress);
+      expect(decodedInput).to.deep.equal(errTestLogs);
+    });
+    it('should throw if there was an error during decoding', () => {
+      expect(() => decodeLogData(errTestLogs, esopAddress)).to.throw('Problem with log data decoding');
+    });
+    it('should throw if address/data is not sent', () => {
+      expect(() => decodeLogData(testLogs)).to.throw('logData or address are undefined');
+    });
+    it('should throw if address was an invalid address', () => {
+      expect(() => decodeLogData(testLogs, invalidAddress)).to.throw('logData or address are undefined');
+    });
   });
 });
