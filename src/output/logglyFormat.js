e6c701baa1b4c614f3d46668ad724df760d7e6ad
Cédric Walter <cedric.walter@gmail.com>
2017-12-18 14:38:02 +0100

    add loggly support

---
+++ src/output/logglyFormat.js
@@ -0,0 +1,35 @@
+const formatLogs = (logs) => {
+  if (!logs) return [];
+
+  return logs.map((log) => {
+    let eventText = `${log.name}(`;
+    log.events.forEach((i, idx, events) => {
+      eventText += `${events[idx].name}=${events[idx].value}`;
+      if (idx !== events.length - 1) {
+        eventText += ',';
+      }
+    });
+    eventText += ')';
+    return eventText;
+  });
+};
+export default (transaction, decodedTransaction, decodedLogs) => ({
+  networkId: transaction.networkId,
+  blockHash: transaction.blockHash,
+  blockNumber: transaction.blockNumber,
+  fromAddress: transaction.from,
+  toAddress: transaction.to,
+  transactionHash: transaction.hash,
+  input: transaction.creates ?
+    decodedTransaction.constructorData : transaction.input,
+  gas: transaction.gas,
+  gasPrice: transaction.gasPrice,
+  status: transaction.status,
+  value: transaction.value,
+  transactionType: transaction.contractAddress ? 'Contract Creation' : 'Transaction',
+  contractAddress: transaction.contractAddress,
+  methodName: decodedTransaction.name,
+  methodParameters: decodedTransaction.params,
+  etherscanLink: `https://etherscan.io/tx/${transaction.hash}`,
+  events: formatLogs(decodedLogs),
+});
